602d9ad818886d5604c6dd8a2bba8107c25a4779
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-01-03 15:37:08 +0900

    feat: 라우터 초기작업 #30

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,18 +1,54 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from '@/pages/Home'
 import Layout from '@/components/Layout/Layout'
+import Login from '@/pages/login'
+import Signup from '@/pages/signup'
+import Mypage from '@/pages/mypage'
+import View from '@/pages/view'
+import NotFound from '@/pages/notfound'
+
+// const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+//   const session = supabase.auth.getSession()
+
+//   if (!session) {
+//     return (
+//       <Navigate
+//         to="/login"
+//         replace
+//       />
+//     )
+//   }
+
+//   return <>{children}</>
+// }
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
     children: [
+      { path: '/', element: <Home /> },
+      { path: '/login', element: <Login /> },
+      { path: '/signup', element: <Signup /> },
       {
-        path: '/',
-        element: <Home />
+        path: '/mypage',
+        element: (
+          // <ProtectedRoute>
+          <Mypage />
+          // </ProtectedRoute>
+        )
+      },
+      {
+        path: '/detail/:id',
+        element: (
+          // <ProtectedRoute>
+          <View />
+          // </ProtectedRoute>
+        )
       }
     ]
-  }
+  },
+  { path: '*', element: <NotFound /> }
 ])
 
 export default function Router() {

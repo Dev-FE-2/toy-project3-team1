e5cf9e4dcbc14206f7ee1e519a2651b3fda73b97
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-01-03 19:23:39 +0900

    feat: 헤더 로고 추가 #24

--- src/components/header/header-main/Header.tsx
+++ src/components/header/header-main/Header.tsx
@@ -1,10 +1,14 @@
 import { IoIosSearch } from 'react-icons/io'
 import * as S from './Header.styled'
+import LogoImage from '@/assets/img/logo/image.png'
 
 export default function Header() {
   return (
     <S.HeaderWrapper>
-      <S.Logo>Floli</S.Logo>
+      <S.Logo
+        src={LogoImage}
+        alt="Floli Logo"
+      />
       <S.SearchIcon>
         <IoIosSearch size={30} />
       </S.SearchIcon>

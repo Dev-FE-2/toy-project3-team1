fc4ee925a3fb61799c794b0705d0e436397682aa
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-01-03 19:14:20 +0900

    style: login style 수정 #24

--- src/pages/Login/login.styles.ts
+++ src/pages/Login/login.styles.ts
@@ -10,47 +10,41 @@ export const Container = styled.div`
 
 export const Logo = styled.img`
   width: 120px;
-  height: auto;
-  margin-bottom: 5rem;
+  margin-bottom: var(--spacing-7);
 `
 
 export const Form = styled.form`
+  margin-top: var(--spacing-3);
   display: flex;
   flex-direction: column;
   width: 80%;
-  max-width: 400px;
+  max-width: var(--max-width);
 `
 
 export const InputWrapper = styled.div`
-  margin-bottom: 1.5rem;
+  margin-bottom: var(--spacing-5);
 `
 
 export const Divider = styled.hr`
-  width: 100%;
-  margin: 1.8rem 0;
+  margin: var(--spacing-5) 0;
   border: none;
-  border-top: 1px solid #ddd;
+  border-top: 1px solid var(--color-border);
 `
 export const SignupText = styled.p`
-  margin-top: 1.5rem;
-  font-size: 1.5rem;
-  color: #333;
+  margin-top: var(--spacing-5);
+  font-size: var(--fs-m);
+  color: var(--color-gray);
   text-align: center;
 `
 
 export const SignupLink = styled.span`
-  color: #6c43e0;
+  color: var(--color-main1);
   font-weight: bold;
   cursor: pointer;
   text-decoration: underline;
-  padding-left: 0.5rem;
-
-  &:hover {
-    color: #5a38c9;
-  }
 `
 export const ErrorMessage = styled.p`
-  font-size: 1.2rem;
+  font-size: var(--fs-s);
   color: red;
-  margin-top: 0.5rem;
+  margin-top: var(--spacing-2);
 `
